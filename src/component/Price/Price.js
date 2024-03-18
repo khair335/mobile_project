0f05a81ad7d4dd5bf8b82b30449f4d16fa8a0dff
sunnyicodelabs <sunny@icodelabs.co>
2024-03-18 21:49:54 +0600

    ..

--- src/component/Price/Price.js
+++ src/component/Price/Price.js
@@ -23,19 +23,22 @@ const Price = () => {
           id: 1,
           price: 1000000,
           status: 'official',
-          variant: '6GB + 128GB'
+          variant: '6GB + 128GB',
+          condition:"new"
         },
         {
           id: 2,
           price: 2000000,
           status: 'unofficial',
-          variant: '12GB + 256GB'
+          variant: '12GB + 256GB',
+          condition:"used"
         },
         {
           id: 3,
           price: 3000000,
           status: 'unofficial',
-          variant: '24GB + 512GB'
+          variant: '24GB + 512GB',
+          condition:"new"
         }
       ]
     },
@@ -49,13 +52,15 @@ const Price = () => {
           id: 2,
           price: 2000000,
           status: 'unofficial',
-          variant: '12GB + 256GB'
+          variant: '12GB + 256GB',
+          condition:"used"
         },
         {
           id: 3,
           price: 3000000,
           status: 'unofficial',
-          variant: '24GB + 512GB'
+          variant: '24GB + 512GB',
+          condition:"used"
         }
       ]
     },
@@ -68,7 +73,8 @@ const Price = () => {
           id: 1,
           price: 1000000,
           status: 'official',
-          variant: '6GB + 128GB'
+          variant: '6GB + 128GB',
+          condition:"used"
         }
       ]
     },
@@ -81,7 +87,8 @@ const Price = () => {
           id: 1,
           price: 1000000,
           status: 'official',
-          variant: '6GB + 128GB'
+          variant: '6GB + 128GB',
+          condition:"used"
         }
       ]
     },
@@ -95,6 +102,7 @@ const Price = () => {
           price: 1000000,
           status: 'official',
           variant: '6GB + 128GB'
+          ,condition:"used",
         }
       ]
     },
@@ -107,7 +115,8 @@ const Price = () => {
           id: 1,
           price: 1000000,
           status: 'official',
-          variant: '6GB + 128GB'
+          variant: '6GB + 128GB',
+          condition:"used"
         }
       ]
     },
@@ -120,7 +129,8 @@ const Price = () => {
           id: 1,
           price: 1000000,
           status: 'official',
-          variant: '6GB + 128GB'
+          variant: '6GB + 128GB',
+          condition:"used"
         }
       ]
     },
@@ -133,7 +143,8 @@ const Price = () => {
           id: 1,
           price: 1000000,
           status: 'official',
-          variant: '6GB + 128GB'
+          variant: '6GB + 128GB',
+          condition:"used"
         }
       ]
     },
@@ -146,7 +157,8 @@ const Price = () => {
           id: 1,
           price: 1000000,
           status: 'official',
-          variant: '6GB + 128GB'
+          variant: '6GB + 128GB',
+          condition:"new"
         }
       ]
     },
@@ -159,315 +171,21 @@ const Price = () => {
           id: 1,
           price: 1000000,
           status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 1,
-      shop_name: 'Rio International',
-      shop_logo: rio_logo,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        },
-        {
-          id: 2,
-          price: 2000000,
-          status: 'unofficial',
-          variant: '12GB + 256GB'
-        },
-        {
-          id: 3,
-          price: 3000000,
-          status: 'unofficial',
-          variant: '24GB + 512GB'
-        }
-      ]
-    },
-    {
-      id: 2,
-      shop_name: 'Gadget N Gadget ',
-      shop_logo: gng_logo,
-      variants: [
-
-        {
-          id: 2,
-          price: 2000000,
-          status: 'unofficial',
-          variant: '12GB + 256GB'
-        },
-        {
-          id: 3,
-          price: 3000000,
-          status: 'unofficial',
-          variant: '24GB + 512GB'
-        }
-      ]
-    },
-    {
-      id: 3,
-      shop_name: 'Sumash Tech',
-      shop_logo: sumash_tech_logo,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 4,
-      shop_name: 'Kry International',
-      shop_logo: kry_logo,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 5,
-      shop_name: 'Goriber Gadget-গরিবের গ্যাজেট',
-      shop_logo: goriber_gadget,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 6,
-      shop_name: 'Gadget & Park',
-      shop_logo: gadget_park,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 7,
-      shop_name: 'Apple Gadgets',
-      shop_logo: apple_gadgets,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 8,
-      shop_name: 'Gadget Bodda- গ্যাজেট বদ্দা',
-      shop_logo: gadget_bodda,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 9,
-      shop_name: 'Jack N Gadget',
-      shop_logo: jack_n_gadget,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 10,
-      shop_name: 'Dazzle',
-      shop_logo: dazzle,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 1,
-      shop_name: 'Rio International',
-      shop_logo: rio_logo,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        },
-        {
-          id: 2,
-          price: 2000000,
-          status: 'unofficial',
-          variant: '12GB + 256GB'
-        },
-        {
-          id: 3,
-          price: 3000000,
-          status: 'unofficial',
-          variant: '24GB + 512GB'
-        }
-      ]
-    },
-    {
-      id: 2,
-      shop_name: 'Gadget N Gadget ',
-      shop_logo: gng_logo,
-      variants: [
-
-        {
-          id: 2,
-          price: 2000000,
-          status: 'unofficial',
-          variant: '12GB + 256GB'
-        },
-        {
-          id: 3,
-          price: 3000000,
-          status: 'unofficial',
-          variant: '24GB + 512GB'
-        }
-      ]
-    },
-    {
-      id: 3,
-      shop_name: 'Sumash Tech',
-      shop_logo: sumash_tech_logo,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 4,
-      shop_name: 'Kry International',
-      shop_logo: kry_logo,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 5,
-      shop_name: 'Goriber Gadget-গরিবের গ্যাজেট',
-      shop_logo: goriber_gadget,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 6,
-      shop_name: 'Gadget & Park',
-      shop_logo: gadget_park,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 7,
-      shop_name: 'Apple Gadgets',
-      shop_logo: apple_gadgets,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 8,
-      shop_name: 'Gadget Bodda- গ্যাজেট বদ্দা',
-      shop_logo: gadget_bodda,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 9,
-      shop_name: 'Jack N Gadget',
-      shop_logo: jack_n_gadget,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
-        }
-      ]
-    },
-    {
-      id: 10,
-      shop_name: 'Dazzle',
-      shop_logo: dazzle,
-      variants: [
-        {
-          id: 1,
-          price: 1000000,
-          status: 'official',
-          variant: '6GB + 128GB'
+          variant: '6GB + 128GB',
+          condition:"new"
         }
       ]
     }
+   
   ]
 
   const indexOfLastShop = currentPage * shopsPerPage;
   const indexOfFirstShop = indexOfLastShop - shopsPerPage;
   const currentShops = shopPriceData.slice(indexOfFirstShop, indexOfLastShop);
 
+  // Change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   const nextPage = () => {
     if (currentPage < Math.ceil(shopPriceData.length / shopsPerPage)) {
       setCurrentPage(currentPage + 1);
@@ -478,53 +196,6 @@ const Price = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
     }
-  };
-
-  // Function to generate pagination buttons with limited page numbers displayed
-  const renderPaginationButtons = () => {
-    const totalPages = Math.ceil(shopPriceData.length / shopsPerPage);
-    const pageNumbers = [];
-    const limit = 2; // Number of pages to show before/after ellipsis
-
-    if (totalPages <= 5) {
-      for (let i = 1; i <= totalPages; i++) {
-        pageNumbers.push(i);
-      }
-    } else {
-      if (currentPage <= limit + 1) {
-        for (let i = 1; i <= currentPage + limit; i++) {
-          pageNumbers.push(i);
-        }
-        pageNumbers.push('...');
-        pageNumbers.push(totalPages);
-      } else if (currentPage >= totalPages - limit) {
-        pageNumbers.push(1);
-        pageNumbers.push('...');
-        for (let i = totalPages - (limit * 2); i <= totalPages; i++) {
-          pageNumbers.push(i);
-        }
-      } else {
-        pageNumbers.push(1);
-        pageNumbers.push('...');
-        for (let i = currentPage - limit; i <= currentPage + limit; i++) {
-          pageNumbers.push(i);
-        }
-        pageNumbers.push('...');
-        pageNumbers.push(totalPages);
-      }
-    }
-
-    return pageNumbers.map((number, index) => (
-      <button
-        key={index}
-        onClick={() => paginate(number)}
-        className={`mx-1 px-3 py-1 rounded-md focus:outline-none ${
-          currentPage === number ? 'bg-gray-600 text-white' : 'bg-gray-300 text-gray-700'
-        }`}
-      >
-        {number}
-      </button>
-    ));
   };
 
   return (
@@ -558,6 +229,11 @@ const Price = () => {
                         d.status
                       }
                     </p>
+                    <p className={`max-w-[75px] cursor-pointer w-full rounded-ss-[6px] rounded-br-[6px] text-white text-center top-[-14px] text-xs flex justify-center items-center left-[-6px]  ${d.condition === 'new' ? 'bg-orange-500 ' : 'bg-cyan-600'} capitalize `}>
+                      {
+                        d.condition
+                      }
+                    </p>
                   </div>))
               }
 
@@ -575,7 +251,15 @@ const Price = () => {
         >
           Previous
         </button>
-        {renderPaginationButtons()}
+        {/* {[...Array(Math.ceil(shopPriceData.length / shopsPerPage)).keys()].map((number) => (
+          <button
+            key={number + 1}
+            onClick={() => paginate(number + 1)}
+            className={`mx-1 px-3 py-1 rounded-md focus:outline-none ${currentPage === number + 1 ? 'bg-gray-600 text-white' : 'bg-gray-300 text-gray-700'}`}
+          >
+            {number + 1}
+          </button>
+        ))} */}
         <button
           onClick={nextPage}
           disabled={currentPage === Math.ceil(shopPriceData.length / shopsPerPage)}
